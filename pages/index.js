1f92e865749c21b647ccd9068d3052c11c5945ed
FavianIbra <ibra.yanuarta@gmail.com>
2023-02-22 11:44:44 +0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import { Fragment } from "react";
+import Hero from "../components/UI/Hero";
+
+export default function Home() {
+  return (
+    <Fragment>
+      <Hero />
+    </Fragment>
+  );
+}
