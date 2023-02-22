8da135bb4d354494cdb8223506e7850feb9088b8
FavianIbra <ibra.yanuarta@gmail.com>
2023-02-22 19:17:08 +0700

    add portfolio

---
+++ components/UI/Portfolio.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "reactstrap";
+import SectionSubtitle from "./SectionSubtitle";
+import classes from "../../styles/portfolio.module.css";
+// import PortfolioItem from "./PortfolioItem";
+
+import portfolioData from "../data/portfolio";
+
+const Portfolio = () => {
+  const [filter, setFilter] = useState("Mobile App");
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    if (filter === "Mobile App") {
+      const filteredData = portfolioData.filter(
+        (item) => item.category === filter
+      );
+
+      setData(filteredData);
+    }
+
+    if (filter === "Web Design") {
+      const filteredData = portfolioData.filter(
+        (item) => item.category === filter
+      );
+
+      setData(filteredData);
+    }
+  }, [filter]);
+
+  const active = `${classes.tab__btn__active}`;
+
+  return (
+    <section id="portfolio">
+      <Container>
+        <Row>
+          <Col lg="6" md="6" className="mb-5">
+            <SectionSubtitle subtitle=" My portfolio site" />
+            <h4 className="mt-4">Showcasing my work and team projects</h4>
+          </Col>
+
+          <Col lg="6" md="6">
+            <div className={`${classes.tab__btns} text-end`}>
+              <button
+                className={` ${
+                  filter === "Mobile App" ? active : ""
+                } secondary__btn text-white`}
+                onClick={() => setFilter("Mobile App")}
+              >
+                Mobile App
+              </button>
+              
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default Portfolio;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
 import Hero from "../components/UI/Hero";
+import Portfolio from "../components/UI/Portfolio";
 
 export default function Home() {
   return (
     <Fragment>
       <Hero />
+      <Portfolio />
     </Fragment>
   );
 }
